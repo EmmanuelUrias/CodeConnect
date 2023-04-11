7b9fd68b675c8f9ef32e685c4865b1b59ac0cc1d
EmmanuelU <emmanuelurias60@icloud.com>
2023-04-11 06:45:20 -0700

    getting started on a new route

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import authRoutes from './routes/auth.js'
 import { register } from './controllers/auth.js'
+import userRoutes from './routes/users.js'
 import { verifyToken } from './middleware/auth.js'
 
 // Configs
@@ -43,6 +44,7 @@ app.post('/auth/register', upload.single('picture', register))
 
 // Routes
 app.use('/auth', authRoutes)
+app.use('/users', userRoutes)
 
 // Mongoose Setup
 const PORT = process.env.PORT || 6001
