ff555a84290b547bef1e3de46d2a27c280633fd3
EmmanuelU <emmanuelurias60@icloud.com>
2023-04-11 06:48:57 -0700

    getting started on users controller

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,3 +7,12 @@ import {
 import { verifyToken } from '../middleware/auth.js'
 
 const router = express.Router()
+
+//  Read
+router.get('/:id', verifyToken, getUser)
+router.get(':id/friends', verifyToken, getUserFriends)
+
+// Update 
+router.patch('/:id/:friendId', verifyToken, addRemoveFriend)
+
+export default router
