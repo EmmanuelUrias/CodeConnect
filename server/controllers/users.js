d5a73136dbdb58f14599b8d21cb452ab9f610237
EmmanuelU <emmanuelurias60@icloud.com>
2023-04-11 07:06:44 -0700

    added update route for friends

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -6,6 +6,7 @@ export const getUser = async (req, res) => {
         const { id } = req.params
         const user = await User.findById(id)
         res.status(200).json(user)
+
     } catch (err) {
         res.status(404).json({ message: err.message })
     }
@@ -24,6 +25,40 @@ export const getUserFriends = async (req, res) => {
             return { _id, firstName, lastName, occupation, location, picturePath }
         })
         res.status(200).json(formattedFriends)
+
+    } catch (err) {
+        res.status(404).json({ message: err.message })
+    }
+}
+
+// Update
+export const addRemoveFriend = async (req, res) => {
+    try {
+        const { id, friendId } = req.params
+        const user = await User.findById(id)
+        const friend = await User.findById(friendId)
+
+        if(user.friends.includes(friendId)) { // This if statement is removing the friend or user if the friendId/id is equal to the id
+            user.friends = user.friends.filter((id) => id !== friendId)
+            friend.friends = friend.friends.filter((id) => id !== id)
+        } else { // adds friends
+            user.friends.push(friendId) 
+            friend.friends.push(id)
+        }
+
+        await user.save()
+        await friend.save()
+
+        const friends = await Promise.all(
+            user.friends.map((id) => User.findById(id))
+        )
+
+        const formattedFriends = friends.map(({ _id, firstName, lastName, occupation, location, picturePath }) => {
+            return { _id, firstName, lastName, occupation, location, picturePath }
+        })
+
+        res.status(200).json(formattedFriends)
+
     } catch (err) {
         res.status(404).json({ message: err.message })
     }
