bc5a169961869bb28e4d79b1913146d82e67a227
EmmanuelU <emmanuelurias60@icloud.com>
2023-04-14 19:48:25 -0700

    made front end routes w/ react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
 import HomePage from 'scenes/homePage';
 import LoginPage from 'scenes/loginPage';
@@ -6,9 +7,17 @@ import ProfilePage from 'scenes/profilePage';
 
 
 function App() {
+  const [mode, setMode] = useState(false)
+
   return (
     <div className="app">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LoginPage />}/>
+          <Route path='/home' element={<HomePage />}/>
+          <Route path='/profile/:userId' element={<ProfilePage />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
