91aa22d2009d44a79d9b5cb49f0f7dabcbc1fff7
EmmanuelU <emmanuelurias60@icloud.com>
2023-04-14 20:42:13 -0700

    implemented redux store into the app storage, NOTES: line 25-28, line 32 is experimental if any errs in the future check back on that, worst case is get an older version of redux toolkit

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import authReducer from './state'
+import { MiddlewareArray, configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react';
+import persistReducer from 'redux-persist/es/persistReducer';
+
+const persistConfig = { key: 'root', storage, version: 1 }
+const persistedReducer = persistReducer(persistConfig, authReducer)
+
+const middleware = new MiddlewareArray().concat({ 
+  serializableCheck: {
+    ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+}})
+
+const store = configureStore({ 
+  reducer: persistedReducer,
+  middleware: middleware
+ })
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persister={persistStore(store)}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
 
