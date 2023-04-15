5b942276564d369125b3ebe2bb5ba887de4a8108
EmmanuelU <emmanuelurias60@icloud.com>
2023-04-15 15:07:47 -0700

    forgot to add this to the last commit

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
     const primaryLight = theme.palette.primary.light
     const alt = theme.palette.primary.alt
 
-    const fullName = `${user.firstName} ${user.lastName}`
+    // const fullName = `${user.firstName} ${user.lastName}`
 
     return (
         <FlexBetween padding='1rem 7%' backgroundColor={alt}>
