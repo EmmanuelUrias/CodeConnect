36d45bb402fe9a9796d10e2e6fd7611b0c292cdc
EmmanuelU <emmanuelurias60@icloud.com>
2023-04-14 21:18:00 -0700

    fixed previous experimental code that caused an err, added imports to App.js, and working on FlexBetween component

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,28 @@ import HomePage from 'scenes/homePage';
 import LoginPage from 'scenes/loginPage';
 import Navbar from 'scenes/navbar';
 import ProfilePage from 'scenes/profilePage';
+import { useMemo } from 'react'
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material';
+import { themeSettings } from 'theme';
 
 
 function App() {
-  const [mode, setMode] = useState(false)
+  const mode = useSelector((state) => state.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
 
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<LoginPage />}/>
-          <Route path='/home' element={<HomePage />}/>
-          <Route path='/profile/:userId' element={<ProfilePage />}/>
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path='/' element={<LoginPage />}/>
+            <Route path='/home' element={<HomePage />}/>
+            <Route path='/profile/:userId' element={<ProfilePage />}/>
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import authReducer from './state'
-import { MiddlewareArray, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux'
 import {
   persistStore,
@@ -17,19 +17,18 @@ import {
 } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import { PersistGate } from 'redux-persist/integration/react';
-import persistReducer from 'redux-persist/es/persistReducer';
 
 const persistConfig = { key: 'root', storage, version: 1 }
 const persistedReducer = persistReducer(persistConfig, authReducer)
 
-const middleware = new MiddlewareArray().concat({ 
-  serializableCheck: {
-    ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
-}})
-
 const store = configureStore({ 
   reducer: persistedReducer,
-  middleware: middleware
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+    }
+  })
  })
 
 
@@ -37,7 +36,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persister={persistStore(store)}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
         <App />
       </PersistGate>
     </Provider>
