aebfc70c60b548f52199964f498a7ceee2a3e09f
EmmanuelU <emmanuelurias60@icloud.com>
2023-04-14 20:06:37 -0700

    redux state is being exported

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    mode: "Light",
+    mode: "light",
     user: null,
     token: null,
     posts: []
@@ -22,6 +22,25 @@ export const authSlice = createSlice({
             state.user = null
             state.token = null
         },
-
+        setFriends: (state, action) => {
+            if (state.user) {
+                state.user.friends = action.payload.friends
+            } else {
+                console.error('no found user friends')
+            }
+        },
+        setPosts: (state, action) => {
+            state.posts = action.payload.posts
+        },
+        setPost: (state, action) => {
+            const updatedPost = state.posts.map((post) => {
+                if (post._id === action.payload.post_id) return action.payload.post
+                return post
+            })
+            state.posts = updatedPost
+        }
     }
 })
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } = authSlice.actions
+export default authSlice.reducer
