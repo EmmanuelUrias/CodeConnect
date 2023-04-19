057cf58022bfd0b4ffc65322b567838b3270733e
EmmanuelU <emmanuelurias60@icloud.com>
2023-04-18 20:28:47 -0700

    finished user widget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,9 +1,11 @@
 import { Box } from "@mui/material"
 import Navbar from "scenes/navbar"
+import UserWidget from "scenes/widgets/UserWidget"
 
 const HomePage = () => {
     return (<Box>
         <Navbar />
+        <UserWidget/>
     </Box>)
 }
 
--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -1,9 +1,7 @@
-import {
-    ManageAccountsOutlined,
-    EditOutLined,
-    LocationOnOutLined,
-    WorkOutLinedOutLined
-} from '@mui/icons-material'
+import ManageAccountsOutlinedIcon from '@mui/icons-material/ManageAccountsOutlined'
+import EditOutLinedIcon from '@mui/icons-material/EditOutlined'
+import LocationOnOutLinedIcon from '@mui/icons-material/LocationOnOutlined'
+import WorkOutLineOutLinedIcon from '@mui/icons-material/WorkOutlineOutlined'
 import { Box, Typography, Divider, useTheme } from '@mui/material'
 import UserImg from 'components/UserImgWidget'
 import FlexBetween from 'components/FlexBetween'
@@ -53,12 +51,78 @@ const UserWidget = ({ userId, picturePath }) => {
                             {friends.length} friends
                         </Typography>
                     </Box>
-                    <ManageAccountsOutlined />
+                    <ManageAccountsOutlinedIcon />
                 </FlexBetween>
+
                 <Divider />
+
+                {/* Second Row */}
+                <Box p='1rem 0'>
+                    <Box display='flex' alignItems='center' gap='1rem' mb='0.5rem'>
+                        <LocationOnOutLinedIcon fontSize='large' sx={{ color: main }}/>
+                        <Typography color={medium}>{location}</Typography>
+                    </Box>
+                    <Box display='flex' alignItems='center' gap='1rem'>
+                        <WorkOutLineOutLinedIcon fontSize='large' sx={{ color: main }}/>
+                        <Typography color={medium}>{occupation}</Typography>
+                    </Box>
+                </Box>
+
+                {/* Third Row */}
+                <Box p='1rem 0'>
+                    <FlexBetween mb='0.5rem'>
+                        <Typography color={medium}>Who's viewed your profile</Typography>
+                        <Typography color={main} fontWeight='500'>{viewedProfile}</Typography>
+                    </FlexBetween>
+                    <FlexBetween>
+                        <Typography color={medium}>Impressions of your post</Typography>
+                        <Typography color={main} fontWeight='500'>{impressions}</Typography>
+                    </FlexBetween>
+                </Box>
+
+                {/* Fourth Row */}
+                <Box p='1rem 0'>
+                    <Typography fontSize='1rem' color={main} fontWeight='500' mb='1rem'>
+                        Social Profiles
+                    </Typography>
+
+                    {/* Twitter Icon */}
+                    <FlexBetween gap='1rem' mb='0.5rem'>
+                        <FlexBetween gap='1rem'>
+                            <img src='../assets/twitter.png' alt='twitter'/>
+                            <Box>
+                                <Typography color={main} fontWeight='500'>
+                                    Twitter
+                                </Typography>
+                                <Typography color={medium}>
+                                    Social Network
+                                </Typography>
+                            </Box>
+                        </FlexBetween>
+                        <EditOutLinedIcon sx={{ color: main }}/>
+                    </FlexBetween>
+
+                    {/* Linkedin Icon */}
+                    <FlexBetween gap='1rem'>
+                        <FlexBetween gap='1rem'>
+                            <img src='../assets/linkedin.png' alt='linkedin'/>
+                            <Box>
+                                <Typography color={main} fontWeight='500'>
+                                    Linkedin
+                                </Typography>
+                                <Typography color={medium}>
+                                    Network Platform
+                                </Typography>
+                            </Box>
+                        </FlexBetween>
+                        <EditOutLinedIcon sx={{ color: main }}/>
+                    </FlexBetween>
+                </Box>
             </FlexBetween>
 
         </WidgetWrap>
     )
 
 }
+
+export default UserWidget
